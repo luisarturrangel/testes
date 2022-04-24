1d2ed6dfc979de09faf2e1b3107ebb439ec8c486
Luis Artur Rangel <91829643+luisarturrangel@users.noreply.github.com>
2022-04-24 10:52:04 -0300

    nem vi isso

--- sites/agenda/main/formulario.php
+++ sites/agenda/main/formulario.php
@@ -4,5 +4,9 @@ $nome = $_POST['nome'];
 $descricao = $_POST['descricao'];
 $marcar = $_POST['marcar'];
 
-echo "nome: $nome descricao: $descricao data: $marcar";
+$mysqli = new mysqli($nome, $descricao, $marcar);
+
+if($mysqli->connect_errno)
+    echo "Falha na conexão: (" .$mysqli->connect_errno.") ".$mysqli->connect_error;
+
 ?>
